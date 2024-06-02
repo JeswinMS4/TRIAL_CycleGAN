3d5ca95a9480e0f931703025d31dcf790e78f773
Jeswin MS <104427327+JeswinMS4@users.noreply.github.com>
2024-06-03 00:05:20 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,9 +1,6 @@
 """
 Training for CycleGAN
 
-Programmed by Aladdin Persson <aladdin.persson at hotmail dot com>
-* 2020-11-05: Initial coding
-* 2022-12-21: Small revision of code, checked that it works with latest PyTorch version
 """
 
 import torch
