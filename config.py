b5c5fd186bae04961b19f121f0d68b7831700ef1
Aniruddh Mantrala <99598258+DarkSamurai1907@users.noreply.github.com>
2024-07-20 10:52:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-TRAIN_DIR = "data/train"
-VAL_DIR = "data/val"
+TRAIN_DIR = "data - real/train"
+VAL_DIR = "data - real/val"
 BATCH_SIZE = 1
 LEARNING_RATE = 1e-5
 LAMBDA_IDENTITY = 0.0
